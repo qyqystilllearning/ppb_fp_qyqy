12e4df4c6d8ba6f39c908d5fb693fa6abbb91ab3
qyqystilllearning <capt.rizqy@gmail.com>
2026-06-18 23:18:44 +0800

    scaffold, appbar

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Rizqys first app lohya')
+      ),
+      body: Text("ini area privasiku privasiku privasiku y")
+    ),
+  ));
+}
