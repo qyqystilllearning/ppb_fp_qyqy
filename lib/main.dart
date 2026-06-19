1754a0616f036079b89e0d51a41014f0df94d1cf
qyqystilllearning <capt.rizqy@gmail.com>
2026-06-19 19:56:44 +0800

    learn widget and myapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
-import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Rizqys first app lohya'),
-        centerTitle: true,
-      ),
-      body: Center(child: Text("ini area privasiku privasiku privasiku y")),
-       floatingActionButton: FloatingActionButton(
-      onPressed: () { },
-      child: Text('ya'),
-       ),
-  )));
-}
+  import 'package:flutter/material.dart';
+
+  void main() => runApp(MyApp());
+
+  class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp( // the whole screen
+        home: Scaffold( // the appbar and body
+          appBar: AppBar( // the appbar (kind of like the header)
+            title: Text("Rizqys first app lohya"),
+            centerTitle: true,
+            backgroundColor: Colors.amber[200],
+          ),
+          body: Center( // the body (the main content)
+            child: Text(
+              'perihal cincin, kucari waktu',
+              style: TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.grey[600],
+              ),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton( // the floating action button (the button that floats on the screen)
+            onPressed: () {},
+            child: Text('y'),
+            backgroundColor: Color(0xFF42A5F5),
+          ),
+        ),
+      );
+    }
+  }
